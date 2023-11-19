4e742916bf6350bc77c28598572dc9b431f3f9fe
Kania Wulandari <144643492+kaniawulandari@users.noreply.github.com>
2023-11-19 15:05:43 +0700

    UTS.js

---
+++ UTS.js
@@ -0,0 +1,20 @@
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
+}
+
+function submitForm() {
+    var form = document.getElementById('reservation-form');
+    var nama = form.elements['nama'].value;
+    var email = form.elements['email'].value;
+    var tanggalCheckin = form.elements['tanggal-checkin'].value;
+    var tanggalCheckout = form.elements['tanggal-checkout'].value;
+    var jumlahTamu = form.elements['jumlah-tamu'].value;
+
+    console.log('Nama:', nama);
+    console.log('Email:', email);
+    console.log('Tanggal Check-in:', tanggalCheckin);
+    console.log('Tanggal Check-out:', tanggalCheckout);
+    console.log('Jumlah Tamu:', jumlahTamu);
+
+}
